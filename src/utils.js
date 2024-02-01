44ff2100c7ad0226e21ffd232bb37d81ae3d7be6
MZuran <mzuran@frba.utn.edu.ar>
2024-02-01 13:35:51 -0300

    Add: JSONWebToken Utils

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,30 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+import { jwt } from 'jsonwebtoken';
+
+export const PRIVATE_KEY = "SecretJSONWebTokenKey"
+
+//JsonWebToken
+export const generateToken = (user) => {
+    const token = jwt.sign({ user }, PRIVATE_KEY, { expiresIn: "24h" })
+    return token
+}
+
+export const authToken = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) return res.status(401).send({ error: 'not authenticated' })
+
+    const token = authHeader.split(' ')[1]
+
+    //Credentials: If the token is successfully verified, the decoded information will be passed to this parameter. 
+    jwt.verify(token, PRIVATE_KEY, (error, credentials) => {
+        if (error) return res.status(403).send({ status: "error", error: "Not authentified" })
+        req.user = credentials.user
+        next()
+    })
+}
+
 //Bcrypt
 import bcrypt from "bcrypt";
 export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
